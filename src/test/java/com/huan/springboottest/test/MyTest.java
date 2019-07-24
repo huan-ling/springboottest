b71e471e45ba9b4ac6cf037b07a103f841e5b6de
wb_xugz <xgz1112@outlook.com>
2019-07-24 17:50:50 +0800

    获取方法的参数

--- src/test/java/com/huan/springboottest/test/MyTest.java
+++ src/test/java/com/huan/springboottest/test/MyTest.java
@@ -2,6 +2,8 @@ package com.huan.springboottest.test;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Random;
 
 /**
@@ -19,9 +21,13 @@ public class MyTest {
             sql += baseSql + new Random().nextInt(10)+" where id="+i+"; ";
         }
         System.out.println(sql);
+        switch (baseSql) {
+            case "1":
+                break;
+        }
     }
 
-    private int getPId(int i){
+    private int getPId(int i,String name){
         return i == 0 ? 0 : i < 10 ? 1: new Random().nextInt(10);
     }
 
@@ -29,4 +35,16 @@ public class MyTest {
     private int getAge(){
         return new Random().nextInt(100);
     }
+
+
+    @Test
+    public void test2() throws NoSuchMethodException {
+        Class<MyTest> clz = MyTest.class;
+        Method method = clz.getDeclaredMethod("getPId", int.class,String.class);
+        Parameter[] parameters = method.getParameters();
+        for (Parameter p : parameters) {
+            System.out.println(p.getName());
+        }
+    }
+
 }
